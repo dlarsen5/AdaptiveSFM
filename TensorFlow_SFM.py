a311818408df1084fd4ada21f680ce99520cee8b
dlarsen5 <dklarsen@live.com>
2017-12-22 21:59:22 -0500

    Added full SFM_train model from paper to do tests with original data
    Added Tensorflow SFM class and added initial methods

--- TensorFlow_SFM.py
+++ TensorFlow_SFM.py
@@ -4,9 +4,6 @@ import numpy as np
 X = np.asarray([[[1,2,3,4],[2,3,4,5],[2,4,31,63]],[[1,2,3,4],[2,3,4,1],[15,35,24,34]]])
 Y = np.asarray([1],[2])
 
-#x = tf.placeholder(tf.float32, shape=X.shape)
-#y = tf.placeholder(tf.float32, shape=Y.shape)
-
 def weight_variable(shape):
     initial = tf.truncated_normal(shape, stddev=0.1)
     return tf.Variable(initial)
@@ -32,6 +29,12 @@ class SFM():
         self.build_eval_graph()
 
     def initialize_weights(self):
+
+        self.global_step = tf.Variable(0, name="global_step")
+        self.Re_s_ = tf.Variable(0)
+        self.Im_s_ = tf.Variable(0)
+        self.Im_s_ = tf.Variable(0)
+        self.omega_ = tf.Variable(0)
 
         self.training_params['W_state'] = tf.Variable(tf.zeros([self.observations, self.sequence_length]))
         self.training_params['V_state'] = tf.Variable(tf.zeros([self.observations, self.input_dim]))
@@ -62,7 +65,7 @@ class SFM():
         self.training_params['V_omega'] = tf.Variable(tf.zeros([self.w_frequencies, self.input_dim]))
         self.training_params['b_omega'] = tf.Variable(tf.zeros([self.w_frequencies]))
 
-    def forward(self, Re_s_, Im_s_, z, x, t_, z_k, omgea_):
+    def forward(self, Re_s_, Im_s_, z, x, t_, z_k, omega_):
 
         state_forget_gate = tf.sigmoid(tf.matmul(self.training_params['W_state'], z) + tf.matmul(self.training_params['V_state'], x) + self.training_params['b_state'], name='State_Forget')
         frequency_forget_gate = tf.sigmoid(tf.matmul(self.training_params['W_freq'], z) + tf.matmul(self.training_params['V_freq'], x) + self.training_params['b_freq'], name='Freq_Forget')
@@ -74,9 +77,9 @@ class SFM():
         modulation_gate = tf.tanh(tf.matmul(self.training_params['W_i'], z) + tf.matmul(self.training_params['V_i'], x) + self.training_params['b_i'],name='Modulation_Gate')
         # for ADP
         omega = tf.matmul(self.training_params['W_omega'], z) + tf.matmul(self.training_params['V_omega'], x) + self.training_params['b_omega']
-
-        real_s = combined_forget_gate * Re_s_ + tf.einsum('i,j->ij', input_gate * modulation_gate, tf.cos(omgea_ * t_))
-        img_s = combined_forget_gate * Im_s_ + tf.einsum('i,j->ij', input_gate * modulation_gate, tf.sin(omgea_ * t_))
+        #todo what the fuck is _t? is it time step cuz then its indexing not an actual number
+        real_s = combined_forget_gate * Re_s_ + tf.einsum('i,j->ij', input_gate * modulation_gate, tf.cos(omega_ * t_))
+        img_s = combined_forget_gate * Im_s_ + tf.einsum('i,j->ij', input_gate * modulation_gate, tf.sin(omega_ * t_))
 
         amplitude = tf.sqrt(real_s ** 2 + img_s ** 2)
 
@@ -86,34 +89,66 @@ class SFM():
 
         zz = []
         A_k = []
-
+        #TODO: figure out how to make zz vector as sum of frequency vectors
         for z_, A_k in zip(z_k,A_k):
             zz.append(z_ + o * tf.tanh(tf.matmul(self.training_params['W_z'], A_k) + self.training_params['b_z']))
 
-        zz = np.array(zz).sum()
+        predicted = np.array(zz).sum()
+        #todo determine how to use true values
+        true = []
 
-        return zz, real_s, img_s, omega
+        return predicted, true, real_s, img_s, omega
 
-    def loss(self):
-        pass
-    def optimize(self):
-        pass
+    def log_loss(self, true, predicted):
+        loss = tf.losses.log_loss(labels=true, predictions=predicted)
+        return loss
+
+    def optimize(self, loss):
+
+        #TODO: set learning rate
+        lr = 0
+        self._lr = lr
+        optimizer = tf.train.GradientDescentOptimizer(lr)
+        train = optimizer.minimize(loss, global_step=self.global_step, gate_gradients=optimizer.GATE_NONE)
+
+        self._train = train
+
     def build_eval_graph(self):
         #build testing graph to predict values and compare to test values
         pass
+
     def build_graph(self):
         #build overall model graph
 
+        predicted, true, self.Re_s_, self.Im_s_, self.omega_ = self.forward(Re_s_, Im_s_, z, x, t_, z_k, omgea_)
+        loss = self.log_loss(true, predicted)
+        self._loss = loss
+        self.optimize(loss)
 
+        tf.global_variables_initializer().run()
 
+        self.saver = tf.train.Saver()
 
-        pass
     def train(self):
         prev_log_like = -np.inf
         omega = np.array([i * 2 * np.pi / train_opt['dim_feq'] for i in range(train_opt['dim_feq'])])
 
-        pass
+        (epoch, step, loss, words, lr) = self._session.run(
+            [self._epoch, self.global_step, self._loss, self._words, self._lr])
+
     def predict(self):
         pass
     def test(self):
         pass
+
+def main():
+    with tf.Graph().as_default(), tf.Session() as session:
+        with tf.device("/cpu:0"):
+            model = SFM(opts, session)
+        for _ in xrange(opts.epochs_to_train):
+            model.train()  # Process one epoch
+            model.eval()  # Eval analogies.
+        # Perform a final save.
+        model.saver.save(session,
+                         os.path.join(opts.save_path, "model.ckpt"),
+                         global_step=model.global_step)
