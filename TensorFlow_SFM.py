b92d7e8f97194344146f7af1fa5f094111001135
dlarsen5 <dklarsen@live.com>
2018-01-14 20:26:44 -0500

    Need to fix optimization but can finally generate correct loss now

--- TensorFlow_SFM.py
+++ TensorFlow_SFM.py
@@ -1,157 +1,184 @@
 import tensorflow as tf
 import numpy as np
-#dimensions 2 x 3 x 4
-X = np.asarray([[[1,2,3,4],[2,3,4,5],[2,4,31,63]],[[1,2,3,4],[2,3,4,1],[15,35,24,34]]])
-Y = np.asarray([1],[2])
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from Get_Data import make_sequences
 
-def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev=0.1)
-    return tf.Variable(initial)
-def bias_variable(shape):
-    initial = tf.constant(0.1, shape=shape)
-    return tf.Variable(initial)
+class SFM_rnn():
 
-class SFM():
+    def __init__(self, state_size, input_size, target_size, model_name):
 
-    def __init__(self,session,observations,sequence_length,w_frequencies,input_dim):
-
-        self._session = session
-        self.training_params = {}
-        # num of training/testing examples
-        self.observations = observations
-        # sequence length, so like features
-        self.sequence_length = sequence_length
-        # frequencies (K) from 1 - K
-        self.w_frequencies = w_frequencies
-        # total input dimensions, so m x n x k
-        self.input_dim = input_dim
+        self.state_size = state_size
+        self.input_size = input_size
+        self.target_size = target_size
+        self.model_name = model_name
         self.build_graph()
-        self.build_eval_graph()
-
-    def initialize_weights(self):
-
-        self.global_step = tf.Variable(0, name="global_step")
-        self.Re_s_ = tf.Variable(0)
-        self.Im_s_ = tf.Variable(0)
-        self.Im_s_ = tf.Variable(0)
-        self.omega_ = tf.Variable(0)
-
-        self.training_params['W_state'] = tf.Variable(tf.zeros([self.observations, self.sequence_length]))
-        self.training_params['V_state'] = tf.Variable(tf.zeros([self.observations, self.input_dim]))
-        self.training_params['b_state'] = tf.Variable(tf.zeros([self.observations]))
-        # frequency forget weights
-        self.training_params['W_freq'] = tf.Variable(tf.zeros([self.w_frequencies, self.sequence_length]))
-        self.training_params['V_freq'] = tf.Variable(tf.zeros([self.w_frequencies, self.input_dim]))
-        self.training_params['b_freq'] = tf.Variable(tf.zeros([self.w_frequencies]))
-        # input weights
-        self.training_params['W_g'] = tf.Variable(tf.zeros([self.observations, self.sequence_length]))
-        self.training_params['V_g'] = tf.Variable(tf.zeros([self.observations, self.input_dim]))
-        self.training_params['b_g'] = tf.Variable(tf.zeros([self.observations]))
-        # information modulation weights
-        self.training_params['W_i'] = tf.Variable(tf.zeros([self.observations, self.sequence_length]))
-        self.training_params['V_i'] = tf.Variable(tf.zeros([self.observations, self.input_dim]))
-        self.training_params['b_i'] = tf.Variable(tf.zeros([self.observations]))
-        # frequency gate weights (k frequencies), omega is output for each frequency layer
-        self.training_params['U_o'] = tf.Variable(tf.zeros([self.w_frequencies, self.sequence_length, self.observations]))
-        self.training_params['W_o'] = tf.Variable(tf.zeros([self.w_frequencies, self.sequence_length, self.sequence_length]))
-        self.training_params['V_o'] = tf.Variable(tf.zeros([self.w_frequencies, self.sequence_length, self.input_dim]))
-        self.training_params['b_o'] = tf.Variable(tf.zeros([self.w_frequencies, self.sequence_length]))
-        # final output weights
-        self.training_params['W_z'] = tf.Variable(tf.zeros([self.w_frequencies, self.sequence_length, self.observations]))
-        self.training_params['b_z'] = tf.Variable(tf.zeros([self.w_frequencies, self.sequence_length]))
-
-        # for Adp
-        self.training_params['W_omega'] = tf.Variable(tf.zeros([self.w_frequencies, self.sequence_length]))
-        self.training_params['V_omega'] = tf.Variable(tf.zeros([self.w_frequencies, self.input_dim]))
-        self.training_params['b_omega'] = tf.Variable(tf.zeros([self.w_frequencies]))
-
-    def forward(self, Re_s_, Im_s_, z, x, t_, z_k, omega_):
-
-        state_forget_gate = tf.sigmoid(tf.matmul(self.training_params['W_state'], z) + tf.matmul(self.training_params['V_state'], x) + self.training_params['b_state'], name='State_Forget')
-        frequency_forget_gate = tf.sigmoid(tf.matmul(self.training_params['W_freq'], z) + tf.matmul(self.training_params['V_freq'], x) + self.training_params['b_freq'], name='Freq_Forget')
-
-        combined_forget_gate = tf.einsum('i,j->ij', state_forget_gate, frequency_forget_gate)
-
-        input_gate = tf.sigmoid(tf.matmul(self.training_params['W_g'], z) + tf.matmul(self.training_params['V_g'], x) + self.training_params['b_g'],name='Input_Gate')
-
-        modulation_gate = tf.tanh(tf.matmul(self.training_params['W_i'], z) + tf.matmul(self.training_params['V_i'], x) + self.training_params['b_i'],name='Modulation_Gate')
-        # for ADP
-        omega = tf.matmul(self.training_params['W_omega'], z) + tf.matmul(self.training_params['V_omega'], x) + self.training_params['b_omega']
-
-        real_s = combined_forget_gate * self.Re_s_ + tf.einsum('i,j->ij', input_gate * modulation_gate, tf.cos(omega_))
-        img_s = combined_forget_gate * self.Im_s_ + tf.einsum('i,j->ij', input_gate * modulation_gate, tf.sin(omega_))
-
-        amplitude = tf.sqrt(real_s ** 2 + img_s ** 2)
-
-        # omega that combines amplitude, previous output, current input, and bias, dimensions M
-        o = tf.sigmoid(tf.matmul(self.training_params['U_o'], amplitude) + tf.matmul(self.training_params['W_o'], z) + tf.matmul(self.training_params['V_o'], x) + self.training_params['b_o'])
-        # output vector for each frequency component
-
-        Re_s_assign = tf.assign(self.Re_s_,real_s)
-        Im_s_assign = tf.assign(self.Im_s_, img_s)
-
-        zz = []
-        A_k = []
-        #TODO: figure out how to make zz vector as sum of frequency vectors
-        for z_, A_k in zip(z_k,A_k):
-            zz.append(z_ + o * tf.tanh(tf.matmul(self.training_params['W_z'], A_k) + self.training_params['b_z']))
-
-        predicted = np.array(zz).sum()
-        #todo determine how to use true values
-        true = []
-
-        return predicted, true, real_s, img_s, omega
-
-    def log_loss(self, true, predicted):
-        loss = tf.losses.log_loss(labels=true, predictions=predicted)
-        return loss
-
-    def optimize(self, loss):
-
-        #TODO: set learning rate
-        lr = 0
-        self._lr = lr
-        optimizer = tf.train.GradientDescentOptimizer(lr)
-        train = optimizer.minimize(loss, global_step=self.global_step, gate_gradients=optimizer.GATE_NONE)
-
-        self._train = train
-
-    def build_eval_graph(self):
-        #build testing graph to predict values and compare to test values
-        pass
 
     def build_graph(self):
-        #build overall model graph
+        """
+        build the tensorflow computation graph
+        expose graph operations as class methods
+        """
+        self._inputs = tf.placeholder(tf.float64, shape=[None, self.state_size, self.input_size])
+        self.ys = tf.placeholder(tf.float64, shape=[None, self.target_size])
 
-        predicted, true, self.Re_s_, self.Im_s_, self.omega_ = self.forward(Re_s_, Im_s_, z, x, t_, z_k, omgea_)
-        loss = self.log_loss(true, predicted)
-        self._loss = loss
-        self.optimize(loss)
+        self.W_state = tf.Variable(np.random.uniform(size=[self.input_size, self.state_size]))
+        self.V_state = tf.Variable(np.random.uniform(size=[self.state_size, self.state_size]))
+        self.b_state = tf.Variable(np.random.uniform(size=[self.state_size]))
 
-        tf.global_variables_initializer().run()
+        self.W_freq = tf.Variable(np.random.uniform(size=[self.input_size, self.state_size]))
+        self.V_freq = tf.Variable(np.random.uniform(size=[self.state_size, self.state_size]))
+        self.b_freq = tf.Variable(np.random.uniform(size=[self.state_size]))
 
-        self.saver = tf.train.Saver()
+        self.W_g = tf.Variable(np.random.uniform(size=[self.input_size, self.state_size]))
+        self.V_g = tf.Variable(np.random.uniform(size=[self.state_size, self.state_size]))
+        self.b_g = tf.Variable(np.random.uniform(size=[self.state_size]))
 
-    def train(self):
-        prev_log_like = -np.inf
-        omega = np.array([i * 2 * np.pi / train_opt['dim_feq'] for i in range(train_opt['dim_feq'])])
+        self.W_i = tf.Variable(np.random.uniform(size=[self.input_size, self.state_size]))
+        self.V_i = tf.Variable(np.random.uniform(size=[self.state_size, self.state_size]))
+        self.b_i = tf.Variable(np.random.uniform(size=[self.state_size]))
 
-        (epoch, step, loss, words, lr) = self._session.run(
-            [self._epoch, self.global_step, self._loss, self._words, self._lr])
+        self.U_o = tf.Variable(np.random.uniform(size=[self.state_size, self.state_size, self.state_size]))
+        self.W_o = tf.Variable(np.random.uniform(size=[self.state_size, self.input_size, self.state_size]))
+        self.V_o = tf.Variable(np.random.uniform(size=[self.state_size, self.state_size, self.state_size]))
+        self.b_o = tf.Variable(np.random.uniform(size=[self.state_size, self.state_size]))
 
-    def predict(self):
-        pass
-    def test(self):
-        pass
+        self.W_z = tf.Variable(np.random.uniform(size=[self.state_size, self.state_size, self.state_size]))
+        self.b_z = tf.Variable(np.random.uniform(size=[self.state_size, self.state_size]))
 
-def main():
-    with tf.Graph().as_default(), tf.Session() as session:
-        with tf.device("/cpu:0"):
-            model = SFM(opts, session)
-        for _ in xrange(opts.epochs_to_train):
-            model.train()  # Process one epoch
-            model.eval()  # Eval analogies.
-        # Perform a final save.
-        model.saver.save(session,
-                         os.path.join(opts.save_path, "model.ckpt"),
-                         global_step=model.global_step)
+        self.W_omega = tf.Variable(np.random.uniform(size=[self.input_size, self.state_size]))
+        self.V_omega = tf.Variable(np.random.uniform(size=[self.state_size, self.state_size]))
+        self.b_omega = tf.Variable(np.random.uniform(size=[self.state_size]))
+
+        self.W_z_z = tf.Variable(np.random.uniform(size=[self.state_size, self.target_size]))
+        self.b_z_z = tf.Variable(np.random.uniform(size=[self.target_size]))
+
+        def step(prev_output, input):
+
+            x_, t_ = input
+
+            omg_, Re_s_, Im_s_, z_ = tf.unstack(prev_output)
+
+            omg_ = omg_[:, :, 1]
+            z_ = z_[:, :, 1]
+
+            state_forget_gate = tf.sigmoid(tf.matmul(x_, self.W_state) + tf.matmul(z_, self.V_state) + self.b_state)
+            frequency_forget_gate = tf.sigmoid(tf.matmul(x_,self.W_freq) + tf.matmul(z_, self.V_freq) + self.b_freq)
+
+            state_forget_gate = tf.expand_dims(state_forget_gate,axis=-1)
+            frequency_forget_gate = tf.expand_dims(frequency_forget_gate, axis=-1)
+            frequency_forget_gate = tf.transpose(frequency_forget_gate,perm=[0,2,1])
+
+            combined_forget_gate = tf.matmul(state_forget_gate, frequency_forget_gate)
+
+            input_gate = tf.sigmoid(tf.matmul(x_, self.W_g) + tf.matmul(z_, self.V_g) + self.b_g)
+
+            modulation_gate = tf.tanh(tf.matmul(x_, self.W_i) + tf.matmul(z_, self.V_i) + self.b_i)
+
+            omega = tf.matmul(x_, self.W_omega) + tf.matmul(z_, self.V_omega) + self.b_omega
+
+            real_c = tf.sin(omg_*t_)
+            img_s = tf.sin(omg_ * t_)
+            input_mod = input_gate * modulation_gate
+            input_mod = tf.expand_dims(input_mod,axis=-1)
+
+            real_c = tf.expand_dims(real_c,axis=-1)
+            img_s = tf.expand_dims(img_s, axis=-1)
+            real_c = tf.transpose(real_c,perm=[0,2,1])
+            img_s = tf.transpose(img_s, perm=[0, 2, 1])
+
+            real_s = combined_forget_gate * Re_s_ + tf.matmul(input_mod, real_c)
+            img_s = combined_forget_gate * Im_s_ + tf.matmul(input_mod, img_s)
+
+            amplitude = tf.sqrt(tf.add(tf.square(real_s),tf.square(img_s)))
+
+            def __freq(z_k,inputs):
+
+                U_k, W_k, V_k, b_k, W_z_k, b_z_k, A_k = inputs
+                o = tf.sigmoid(tf.matmul(A_k, U_k) + tf.matmul(x_, W_k) + tf.matmul(z_, V_k) + b_k)
+                zz = z_k + o * tf.tanh(tf.matmul(A_k, W_z_k) + b_z_k)
+
+                return tf.stack(zz)
+
+            amplitude = tf.transpose(amplitude,perm=[1,0,2])
+
+            new_z = tf.scan(__freq,elems=[self.U_o,self.W_o,self.V_o,self.b_o,self.W_z,self.b_z,amplitude],initializer=tf.zeros(tf.shape(z_),dtype=tf.float64))
+            #Get last output of iterating through summation of frequency components
+            new_z = new_z[-1]
+
+            omega = tf.stack([omega for _ in range(self.state_size)],axis=1)
+            new_z = tf.stack([new_z for _ in range(self.state_size)], axis=1)
+
+            return tf.stack([omega, real_s, img_s, new_z])
+
+        self.initial_hidden_time = tf.reduce_sum(self._inputs,2)
+        self.initial_hidden_time = tf.expand_dims(self.initial_hidden_time,axis=-1)
+        self.initial_hidden_time = tf.matmul(self.initial_hidden_time,tf.transpose(self.initial_hidden_time,perm=[0,2,1]))
+        self.initial_hidden_time = tf.multiply(self.initial_hidden_time,tf.zeros([self.state_size, self.state_size],dtype=tf.float64))
+
+        self.initial_hidden = tf.stack([self.initial_hidden_time, self.initial_hidden_time, self.initial_hidden_time, self.initial_hidden_time],axis=0)
+
+        #x has size [batch_size, sequence_length, N-features
+        self.processed_input = tf.transpose(tf.transpose(self._inputs,perm=[2,0,1]))
+        outputs = tf.scan(step, elems=[self.processed_input,(np.arange(self.state_size, dtype=np.float64) + 1) / self.state_size], initializer=self.initial_hidden)
+        #last state of hidden state matrices
+        last_state = tf.squeeze(outputs[-1:, -1:, :, -1:],axis=[0,1,3])
+        #combine multifrequency states into one aggregate
+        logits = tf.matmul(last_state, self.W_z_z) + self.b_z_z
+        predictions = tf.nn.softmax(logits)
+        cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=self.ys,logits=logits)
+        loss = tf.reduce_mean(cross_entropy)
+
+        train_op = tf.train.AdamOptimizer().minimize(loss)
+
+        self.predictions = predictions
+        self.loss = loss
+        self.train_op = train_op
+
+    def train(self, train_set, epochs=100, batch_size=100):
+
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            train_loss = 0
+            try:
+                for i in range(epochs):
+                    xs, ys = train_set
+                    for offset in range(0, len(xs), batch_size):
+                        batch_x = xs[offset: offset + batch_size]
+                        batch_y = ys[offset: offset + batch_size]
+                        #TODO: train step makes cross_entropy NAN
+                        #_, train_loss_ = sess.run([self.train_op,self.loss], feed_dict={self._inputs : batch_x, self.ys : batch_y})
+                        train_loss_ = sess.run(self.loss,feed_dict={self._inputs: batch_x, self.ys: batch_y})
+                        train_loss += train_loss_
+                    print('[{}] loss: {}'.format(i,train_loss/100))
+                    train_loss = 0
+            except KeyboardInterrupt:
+                print('Interrupted by user')
+
+X, Y = make_sequences(['AAPL'])
+
+#X = np.random.rand(1000, 8, 20)
+#seq_labels = np.random.choice([True,False],size=[1000, 2])
+
+def get_on_hot(number):
+    on_hot = [0] * 10
+    on_hot[number] = 1
+    return on_hot
+
+digits = datasets.load_digits()
+#X = digits.images
+#Y_ = digits.target
+
+#Y = [get_on_hot(x) for x in Y_]
+
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.22)
+
+# Cuttting for simple iteration
+X_train = X_train[:1400]
+y_train = y_train[:1400]
+
+X_train = np.array(X_train)
+y_train = np.array(y_train)
+
+SFM = SFM_rnn(state_size=X_train.shape[1],input_size=X_train.shape[2], target_size=y_train.shape[1], model_name='SFM_1')
+SFM.train(train_set=[X_train,y_train],epochs=10)
